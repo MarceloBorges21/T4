82f9abccaafd50790715bb97423573e58522ebb6
Estagiario 030 <stag030@benner.com.br>
2019-07-19 17:16:16 -0300

    fim vue1

--- vue.js 1/alurapic/src/App.vue
+++ vue.js 1/alurapic/src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo" />
-      </li>   
-    </ul>
-
+  <div class="corpo">
+    <meu-menu :rotas="routes"/>
+    <transition name="pagina">
+    <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
+
 export default {
-  data() {
-    return {
-      titulo: "AluraPic",
-      fotos: []
-    }
+
+  components:{
+    'meu-menu': Menu
   },
-  created(){
-    let promise= this.$http.get('http://localhost:3000/v1/fotos');
-    promise.then(res => res.json())
-    .then(fotos => this.fotos = fotos);
+
+  data(){
+    return{
+      routes
     }
   }
+}
 </script>
 
 <style>
+
+.corpo{
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.pagina-enter, .pagina-leave-active{
+    opacity:0;
+}
+
+.pagina-enter-active, .pagina-leave-active{
+    transition: opacity .4s;
+}
 </style>
